9536b0c0d61ae1131813a97a346b9d2140dcf39c
ccases <carlosmatthewcases@gmail.com>
2021-12-02 20:58:57 +0800

    reinstalled webpacker, and testing now works

--- spec/views/article_spec.rb
+++ spec/views/article_spec.rb
@@ -5,13 +5,20 @@ RSpec.describe "Articles", type: :feature do
     before {visit articles_path}
 
     it 'shows all articles in the body' do
-      within 'body' do
-        expect(page).to have_content("Hello World")
-        expect(page).to have_content("New Article")
-      end
+      expect(page).to have_content("Hello World")
+      expect(page).to have_content("New Article")
     end
 
     it 'successfully creates a new article' do
+      click_link('New Article')
+      fill_in "article_author", with: "x-author"
+      fill_in "article_content", with: "xzxcz"
+      fill_in "article_title", with: "ASDASx"
+      click_button('Create Article')
+
+      expect(page).to have_content("xzxcz")
+      expect(page).to have_content("ASDASx")
+      expect(page).to have_current_path(articles_path)
     end
   end
 end
